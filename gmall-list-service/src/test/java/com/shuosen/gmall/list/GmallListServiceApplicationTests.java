9bae39e9482a4c9936ca9664f57a4be6b85c1d0c
daniel <593036108@qq.com>
2019-04-18 10:02:56 +0800

    *在测试类中测试 es  通过jestclient客户端  支持dsl语句

---
+++ gmall-list-service/src/test/java/com/shuosen/gmall/list/GmallListServiceApplicationTests.java
@@ -0,0 +1,52 @@
+package com.shuosen.gmall.list;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmallListServiceApplicationTests {
+
+    @Autowired
+    JestClient jestClient;
+
+    @Test
+    public void testEs() throws IOException {
+        /****
+         * 从get以下的部分开始复制
+         * GET /movie_chn/movie/_search
+         * {
+         *   "query": {
+         *     "term": {
+         *       "actorList.name": "张译"
+         *     }
+         *   }
+         * }
+         */
+        String query = "{\n" +
+                "  \"query\": {\n" +
+                "    \"term\": {\n" +
+                "      \"actorList.name\": \"张译\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        Search search = new Search.Builder(query).addIndex("movie_chn").addType("movie").build();
+        SearchResult result = jestClient.execute(search);
+        List<SearchResult.Hit<HashMap, Void>> hits = result.getHits(HashMap.class);
+        for (SearchResult.Hit<HashMap, Void> hit : hits) {
+            HashMap source = hit.source;
+            System.out.println(source);
+        }
+    }
+
+}
